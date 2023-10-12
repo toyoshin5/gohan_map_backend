ac395edc6f9db566a38c47cdcb2aa8216c9afae6
karintou8710 <karintou8710@gmail.com>
2023-10-13 02:47:15 +0900

    [add] logger

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+import logging
 
+from app.utils.logger import init_logger
+
+# 初期化
+init_logger()
+
+logger = logging.getLogger("gohan_map")
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    logger.debug("request: /")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    logger.debug(f"request: /items/{item_id}")
     return {"item_id": item_id, "q": q}
---
+++ app/utils/logger.py
@@ -0,0 +1,34 @@
+from logging import config
+
+debug_logger_config = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": '[%(levelname)s] %(asctime)s - %(filename)s:%(lineno)ss  - %(message)s'
+        }
+    },
+    "handlers": {
+        "consoleHandler": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {
+        "gohan_map": {
+            "level": "DEBUG",
+            "handlers": ["consoleHandler"],
+            "propagate": False,
+        },
+    },
+
+    "root": {
+        "level": "INFO",
+        "handlers": []
+    }
+}
+
+
+def init_logger():
+    config.dictConfig(debug_logger_config)
