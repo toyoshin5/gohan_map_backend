a249686880477bac36ecadae060bf66ecb19068b
karintou8710 <karintou8710@gmail.com>
2023-10-15 16:36:00 +0900

    [format]

--- alembic/versions/2ad145911582_create_tables.py
+++ alembic/versions/2ad145911582_create_tables.py
@@ -7,9 +7,9 @@ Create Date: 2023-10-15 16:29:02.747362
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '2ad145911582'
